0d12d7f6c87a0412149c05698d286e15a71a951a
Beni Pintea <b.pintea04@gmail.com>
2025-01-03 22:14:25 +0200

    small refactor

--- arduino/morse.esp/morse.esp.ino
+++ arduino/morse.esp/morse.esp.ino
@@ -1,21 +1,27 @@
 #include <WiFi.h>
 #include <ArduinoHttpClient.h>
 #include "secrets.h"
+#include "utils.h"
 
-const char* ssid = SECRET_SSID;
-const char* password = SECRET_PASS;
-const char* host = "192.168.240.197";
-int port = 3000;
+const char* ssid = SSID;
+const char* password = PASSWORD;
+const char* host = SERVER_HOST;
+int port = SERVER_PORT;
 int wifiStatus = WL_IDLE_STATUS;
 
-WiFiClient  wifiClient;
+WiFiClient wifiClient;
 HttpClient http = HttpClient(wifiClient, host, port);
-
 
 void setup() {
   Serial.begin(9600);
+
+  Serial.printf("Connecting to network %s\n", ssid);
   connectToWifi();
-  sendPostRequest();
+  Serial.printf("Connected to network");
+  
+  Serial.println("Sending POST request");
+  String response = sendPostRequest("/api/words", http, "{\n\t\"value\": \"beni\"\n}");
+  Serial.printf("Received from server %s", response);
 }
 
 void loop() {
@@ -23,50 +29,12 @@ void loop() {
 }
 
 
-String get_wifi_status(int status){
-    switch(status){
-        case WL_IDLE_STATUS:
-        return "WL_IDLE_STATUS";
-        case WL_SCAN_COMPLETED:
-        return "WL_SCAN_COMPLETED";
-        case WL_NO_SSID_AVAIL:
-        return "WL_NO_SSID_AVAIL";
-        case WL_CONNECT_FAILED:
-        return "WL_CONNECT_FAILED";
-        case WL_CONNECTION_LOST:
-        return "WL_CONNECTION_LOST";
-        case WL_CONNECTED:
-        return "WL_CONNECTED";
-        case WL_DISCONNECTED:
-        return "WL_DISCONNECTED";
-    }
-}
-
 void connectToWifi() {
-  Serial.print("Connecting to network: ");
-  Serial.print(ssid);
-  Serial.print(" with password: ");
-  Serial.println(password);
   WiFi.begin(ssid, password);
 
   while ( wifiStatus != WL_CONNECTED ) {
     wifiStatus = WiFi.status();
-    Serial.print("status: ");
-    Serial.println(get_wifi_status(wifiStatus));
+    Serial.printf("WiFi status: %s\n", convertWiFiStatusToString(wifiStatus));
     delay(500);
   }
-
-  Serial.print("connected to network: ");
-  Serial.println(WiFi.SSID());
-}
-
-void sendPostRequest() {
-  String payload = "{\n\t\"value\": \"beni\"\n}";
-  String contentType = "application/json";
-  Serial.println("Sending POST request...");
-  http.post("/api/words", contentType, payload);
-
-  int statusCode = http.responseStatusCode();
-  String response = http.responseBody();
-  Serial.print(response);
 }
---
+++ arduino/morse.esp/utils.h
@@ -0,0 +1,26 @@
+#include <ArduinoHttpClient.h>
+
+String convertWiFiStatusToString(int status) {
+  switch(status){
+      case WL_IDLE_STATUS:
+        return "WL_IDLE_STATUS";
+      case WL_SCAN_COMPLETED:
+        return "WL_SCAN_COMPLETED";
+      case WL_NO_SSID_AVAIL:
+        return "WL_NO_SSID_AVAIL";
+      case WL_CONNECT_FAILED:
+        return "WL_CONNECT_FAILED";
+      case WL_CONNECTION_LOST:
+        return "WL_CONNECTION_LOST";
+      case WL_CONNECTED:
+        return "WL_CONNECTED";
+      case WL_DISCONNECTED:
+        return "WL_DISCONNECTED";
+    }
+}
+
+String sendPostRequest(String url, HttpClient client, String body) {
+  String contentType = "application/json";
+  client.post(url, contentType, body);
+  return client.responseBody();
+}
