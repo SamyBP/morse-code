f026a3e55b6178cb52083f44860e28785a3efcc9
Beni Pintea <b.pintea04@gmail.com>
2025-01-03 22:59:06 +0200

    updated script, sends data through the websocket

--- scripts/main.py
+++ scripts/main.py
@@ -1,21 +1,22 @@
-import os
-import serial_asyncio
 import asyncio
+import websockets
+import argparse 
 
-from protocols.http import send_word_to_server
-from protocols.serial import SerialPortReaderProtocol
-
-
-SERVER_WORDS_API = os.getenv('SERVER_WORDS_API', default='http://127.0.0.1:3000/api/words')
-reader_protocol = SerialPortReaderProtocol(handler=send_word_to_server, url=SERVER_WORDS_API)
-
-async def reader(port: str):
-    _, protocol = await serial_asyncio.create_serial_connection(loop, lambda: reader_protocol, port, baudrate=9600)
-
-    while True:
-        await asyncio.sleep(0.3)
-        protocol.resume_reading()
+async def send_word_to_server(uri: str, word: str):
+    if not word:
+        return
     
-loop = asyncio.get_event_loop()
-loop.run_until_complete(reader('COM5'))
-loop.close()
+    async with websockets.connect(uri) as websocket:
+        await websocket.send(word)
+
+        
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--uri', type=str, default='ws://localhost:8080', help='the uri used to establish a websocket connection')
+    parser.add_argument('--word', type=str, help='the word to be send')
+    args = parser.parse_args()
+    asyncio.get_event_loop().run_until_complete(send_word_to_server(uri=args.uri, word=args.word))
+    
+
+if __name__ == '__main__':
+    main()
