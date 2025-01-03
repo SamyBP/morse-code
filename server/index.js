4946a70a6846d15aff4206e605f1d7b3e92b8fd0
Beni Pintea <b.pintea04@gmail.com>
2025-01-03 15:03:13 +0200

    implemented real time word update

--- server/index.js
+++ server/index.js
@@ -1,9 +1,30 @@
+require('dotenv').config();
+
 const express = require('express')
 const path = require('path');
 const bodyParser = require('body-parser')
-const router = require('./api/words')
+const wordsRepository = require('./db/words');
+const WebSocket = require('ws');
 
-const app = express()
+
+const wss = new WebSocket.Server({ port: 8080 })
+
+wss.on('connection', (ws) => {
+    console.log('New websocket connection established on port 8080');
+
+    ws.on('message', (message) => {
+        console.log('Received message:', message.toString());
+
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message.toString());
+            }
+        })
+    })
+})
+
+
+const app = express();
 const PORT = 3000;
 
 app.use(express.json())
@@ -11,7 +32,36 @@ app.use(bodyParser.json())
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/words', router)
+const socket = new WebSocket('ws://localhost:8080');
+
+app.post('/api/words', async (request, response) => {
+    try {
+        const {value} = request.body;
+        console.log(`POST /api/words "value": ${value}`)
+
+        if (!value){
+            const errorResponse = {
+                status: 400,
+                message: 'Invalid body, "value" is required'
+            }
+            console.error(errorResponse);
+            return response.status(400).json(errorResponse);
+        }
+
+        const word = await wordsRepository.save(value);
+        socket.send(JSON.stringify(word));
+        console.log(`Sending response: ${word}`)
+        return response.status(200).json(word);
+
+    } catch (error) {
+        const errorResponse = {
+            status: 400,
+            message: error.message
+        }
+        console.error(errorResponse);
+        return response.status(400).json(errorResponse);
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server running at http://127.0.0.1:${PORT}`);
--- server/public/index.js
+++ server/public/index.js
@@ -1,20 +1,11 @@
-document.getElementById('load-words').addEventListener('click', async () => {
-    try {
-        const response = await fetch('/api/words');
-        if (response.ok) {
-            const words = await response.json();
-            const wordsList = document.getElementById('words-list');
-            wordsList.innerHTML = '';
-            words.forEach(word => {
-                console.log(word);
-                const li = document.createElement('li');
-                li.textContent = word.value;
-                wordsList.appendChild(li);
-            });
-        } else {
-            console.error('Failed to fetch words');
-        }
-    } catch (error) {
-        console.error('Error fetching words:', error);
-    }
-});
+const words = document.getElementById('words-list');
+
+const socket = new WebSocket(`ws://localhost:8080`);
+
+socket.onmessage = (event) => {
+    console.log('hello');
+    const word = event.data;
+    const li = document.createElement('li');
+    li.textContent = word;
+    words.appendChild(li);
+}
