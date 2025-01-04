1a8c636526fe5aebcef2a107749889b3526d5e47
Beni Pintea <b.pintea04@gmail.com>
2025-01-04 17:33:47 +0200

    designed the game and implemented functionalities

--- server/public/index.js
+++ server/public/index.js
@@ -1,11 +1,59 @@
-const words = document.getElementById('words-list');
+const generateWordButton = document.getElementById('generate-word-btn');
+const randomWordDisplay = document.getElementById('word-display');
+const guessStatusDisplay = document.getElementById('guess-status-display');
+const socket = new WebSocket('ws://localhost:8080');
 
-const socket = new WebSocket(`ws://localhost:8080`);
+generateWordButton.addEventListener('click', async () => {
+    try {
+        const response = await fetch('/api/words');
+        const data = await response.json();
+        if (!response.ok) {
+            throw new Error(JSON.stringify(data));
+        }
+
+        localStorage.setItem('generatedWordId', data.id.toString());
+        randomWordDisplay.textContent = data.value;
+        generateWordButton.style.display = 'none';
+        randomWordDisplay.classList.remove('hidden');
+
+    } catch (error) {
+        console.error(error.message);
+        alert('Ran into an error while generating the word. Please try again later');
+    }
+});
 
 socket.onmessage = (event) => {
-    console.log('hello');
-    const word = event.data;
-    const li = document.createElement('li');
-    li.textContent = word;
-    words.insertBefore(li, words.firstChild);
+    const wordToGues = randomWordDisplay.textContent
+    const guessedWord = event.data;
+    const wordId = Number.parseInt(localStorage.getItem('generatedWordId'));
+    
+    if (!wordId || !wordToGues) {
+        console.warn("Need to generate a word first");
+        return;
+    }
+
+    const isCorrect = wordToGues === guessedWord;
+    const guessStatus = isCorrect ? 'correct' : 'wrong'; 
+    guessStatusDisplay.textContent = `Your guess is ${guessStatus}`;
+    guessStatusDisplay.classList.remove('hidden');
+    guessStatusDisplay.classList.add(guessStatus);
+    console.log(`Your guess is ${guessStatus}`);
+
+    const sendGuessToServer = async (id, flag) => {
+        try {
+            const response = await fetch('/api/guess', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({wordId: id, isCorrect: flag})
+            })
+    
+            if (!response.ok) {
+                throw new Error('Error when saving the guess to db');
+            }
+        } catch(error) {
+            console.error(error.message);
+        }
+    }
+
+    sendGuessToServer(wordId, isCorrect);
 }
