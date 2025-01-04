21fcb358c376b7e4fd0b1381396ee749faf412c3
Beni Pintea <b.pintea04@gmail.com>
2025-01-04 14:45:04 +0200

    added setup script for db, modified db schema

---
+++ scripts/setup_db.py
@@ -0,0 +1,63 @@
+import json
+import os
+import psycopg2
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+connection_args = {
+    'dbname': os.getenv("PG_DATABASE"),
+    'user': os.getenv("PG_USER"),
+    'password': os.getenv("PG_PASSWORD"),
+    'host': os.getenv("PG_HOST"),
+    'port': os.getenv("PG_PORT")
+}
+
+def load_resource(file: str) -> dict:
+    with open(file, 'r') as resource:
+        return json.load(resource)
+
+
+def compute_create_table_query(table: dict) -> str:
+    name = table.get('name')
+    columns = table.get('columns')
+    constraints = table.get('constraints')
+    
+    query = f'create table if not exists {name} ('
+    query += ', '.join(columns)
+    
+    if constraints:
+        query += ', ' + ', '.join(constraints)
+    
+    query += ');'
+    return query
+
+
+def create_tables(connection, tables: list[dict]):
+    with connection.cursor() as cursor:
+        for table in tables:
+            query = compute_create_table_query(table)
+            cursor.execute(query)
+    
+
+
+def save_words(connection, words: list[str]):
+    with connection.cursor() as cursor:
+        query = 'insert into word(value) values(%s)'
+        cursor.executemany(query, [(word,) for word in words])
+
+
+def main() -> None:
+    with psycopg2.connect(**connection_args) as connection:
+        schema = load_resource(file='resources/schema.json')
+        data = load_resource(file='resources/words.json')
+        create_tables(connection, tables=schema['tables'])
+        save_words(connection, words=data['words'])
+        connection.commit()
+
+
+if __name__ == '__main__':
+    main()
+
+
--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ wss.on('connection', (ws) => {
 
     ws.on('message', (message) => {
         console.log('socket received message:', message.toString());
-
+        
         wss.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(message.toString());
