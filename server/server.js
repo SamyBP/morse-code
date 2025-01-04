70a72840c0d721bcd288a294f0415f7793623ba6
Beni Pintea <b.pintea04@gmail.com>
2025-01-04 22:24:52 +0200

    small refactor, removed unnecesary POST, added node scripts

--- server/server.js
+++ server/server.js
@@ -1,31 +1,14 @@
 require('dotenv').config();
+// require("./ws.server");
 
 const express = require('express')
 const path = require('path');
 const bodyParser = require('body-parser')
-const WebSocket = require('ws');
 const wordsRouter = require('./api/words/index')
 const guessRouter = require('./api/guess/index')
 
-const wss = new WebSocket.Server({ port: 8080 })
-
-wss.on('connection', (ws) => {
-    console.log('New websocket connection established on port 8080');
-
-    ws.on('message', (message) => {
-        console.log('socket received message:', message.toString());
-
-        wss.clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(message.toString());
-            }
-        })
-    })
-})
-
-
 const app = express();
-const PORT = 3000;
+const PORT = process.env.SERVER_PORT || 3000;
 
 app.use(express.json())
 app.use(bodyParser.json())
---
+++ server/ws.server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const WebSocket = require('ws');
+
+const port = process.env.WS_SERVER_PORT || 8080;
+
+const wss = new WebSocket.Server({ port: port })
+
+wss.on('listening', () => {
+    console.log(`Server listening on ${port}`);
+})
+
+wss.on('connection', (ws) => {
+    console.log(`New websocket connection established`);
+
+    ws.on('message', (message) => {
+        console.log('socket received message:', message.toString());
+
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message.toString());
+            }
+        })
+    })
+})
